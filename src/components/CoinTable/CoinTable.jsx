5d889cd439a83ec5c3d16e4a362e250affa2ef61
Ayush Shaw <ayushshaw590@gmail.com>
2026-08-08 13:15:14 +0530

    adding react infinite scroling/pagination

--- src/components/CoinTable/CoinTable.jsx
+++ src/components/CoinTable/CoinTable.jsx
@@ -1,37 +1,77 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { fetchCoinData } from "../../services/fetchCoinData";
 import { useQuery } from "@tanstack/react-query";
 
 function CoinTable() {
-
   const [page, setPage] = useState(1);
 
- const { data, isLoading, isError, error } = useQuery({
-  queryKey: ["coins", page],
-  queryFn: () => fetchCoinData(page, "usd"),
-  retry: 2,
-  retryDelay: 1000,
-  gcTime: 1000 * 60 * 2, // cacheTime was renamed to gcTime
-});
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ["coins", page],
+    queryFn: () => fetchCoinData(page, "usd"),
+    //   retry: 2,
+    //   retryDelay: 1000,
+    gcTime: 1000 * 60 * 2, // cacheTime was renamed to gcTime
+    staleTime: 1000 * 60 * 2, // if data is already in cache then it not request api call
+  });
 
-useEffect(() => {
-    console.log(data);
-  }, [data]);
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
   if (isError) {
     return <div>Error: {error.message}</div>;
   }
 
-  
+  return (
+    <div className="my-5 flex flex-col items-center justify-center gap-5 w-[80vw] mx-auto">
+      <div className="flex items-center justify-center w-full px-2 py-4 font-semibold text-black bg-yellow-400 ">
+        {/* header of the table */}
+        <div className="basis-[35%]">Coin</div>
+        <div className="basis-[25%]">Price</div>
+        <div className="basis-[20%]">24h change</div>
+        <div className="basis-[20%]">Market Cap</div>
+      </div>
 
-  return(
-  <>
-  Coin Table <button onClick={() => setPage(page+1)}>Click</button>
-  {page}
-  </>
-  )
+      <div className="flex flex-col w-[80vw] mx-auto">
+        {isLoading && <div>loading...</div>}
+        {data &&
+          data.map((coin) => {
+            return (
+              <div
+                key={coin.id}
+                className="flex items-center justify-between w-full px-2 py-4 font-semibold text-white bg-transparent"
+              >
+                <div className=" flex items-center justify-start gap-3 basis-[35%]">
+                  <div className="w-[5rem] h-[5rem]">
+                    <img src={coin.image} className="w-full h-full" />
+                  </div>
+                  
+                  <div className="flex flex-col">
+                    <div className="text-3xl">{coin.name}</div>
+                    <div className="text-xl">{coin.sybol}</div>
+                  </div>
+                </div>
+
+                <div className="basis-[25%]">{coin.current_price}</div>
+                <div className="basis-[20%]">{coin.price_change_24h}</div>
+                <div className="basis-[20%]">{coin.market_cap}</div>
+              </div>
+            );
+          })}
+      </div>
+
+      <div className="flex items-center justify-center gap-4">
+        <button
+          disabled={page === 1}
+          onClick={() => setPage(page - 1)}
+          className="text-2xl text-white btn btn-primary btn-wide"
+        >
+          Prev
+        </button>
+        <button
+          onClick={() => setPage(page + 1)}
+          className="text-2xl text-white btn btn-secondary btn-wide"
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
 }
 export default CoinTable;
